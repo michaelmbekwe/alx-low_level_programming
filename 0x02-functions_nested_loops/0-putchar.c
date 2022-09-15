3ee0228355e2a35ad0c826bc70d3e5968e0b5937
michaelmbekwe <michaelmbekwe@gmail.com>
2022-09-15 12:12:12 +0000

    my second commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - A program that prints _putcher, followed by a new line
- *
- * Return: 0 if successful
- */
+* main - A program that prints _putcher, followed by a #include
+* Return: nothing, retun void
+*/
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+
+void print_alphabet(void);
+
+#endif
