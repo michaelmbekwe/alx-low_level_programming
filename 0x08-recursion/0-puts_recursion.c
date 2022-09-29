003ebafb63de69b52f0831eee5c37bd4f502d685
michaelmbekwe <michaelmbekwe@gmail.com>
2022-09-29 03:12:13 +0000

    my first

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include "stdio.h"
 
-/** 
+/**
  * _puts_recursion -prints a string, followed by a new line
  * @s: string to print
  */
 
 void _puts_recursion(character(char *s)
-	
+
 		{
 		if (*s == '\0')
 		}
