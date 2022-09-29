cbb9a573e82f9caef794ab7c8a53682d209d89a9
michaelmbekwe <michaelmbekwe@gmail.com>
2022-09-29 03:22:43 +0000

    my first

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
-#ifndefine 
-#define
+#ifndef MAIN_H_INCLUDED 
+#define MAIN_H_INCLUDED
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
