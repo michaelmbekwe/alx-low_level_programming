9e6a907fec88d97456c893a02961ec95bbc47c09
michael mbekwe <mbekwemichael1@gmail.com>
2023-06-23 17:25:07 +0000

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Test function for positive or negative
- * Return 0
+ * Return: 0
  */
 
 int main(void)
@@ -10,7 +10,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 		return (0);
