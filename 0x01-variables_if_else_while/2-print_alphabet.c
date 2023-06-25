92d7c8d5da6b45cf85b6a517c286d4b5ad168d8a
michael mbekwe <mbekwemichael1@gmail.com>
2023-06-25 04:17:49 +0000

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the code
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+int letter = 'a';
+
+while (letter <= 'z')
+{
+	putchar(letter);
+
+	letter++;
+}
+	putchar('\n');
+
+	return (0);
+
+}
