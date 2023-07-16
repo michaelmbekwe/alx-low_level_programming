aa6946e7932137bdae6d52a4c6cd81ead08e14fe
michael mbekwe <mbekwemichael1@gmail.com>
2023-07-16 12:12:04 +0000

    still recursing

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_pow_recursion - is a function that calculate number raised to power
+ *@x: is the number
+ *@y: is the power raised
+ *Returns: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+	return (-1);
+}
+	else if (y == 0)
+{
+	return (1);
+}
+	else
+{
+	return (x * _pow_recursion(x, y - 1));
+			}
+			}
