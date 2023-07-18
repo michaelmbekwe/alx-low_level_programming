cb569340f77e056f4481ede1f19d1d7a6d589811
michael mbekwe <mbekwemichael1@gmail.com>
2023-07-18 21:34:32 +0000

    polindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,30 +3,36 @@
 # include <string.h>
 
 /**
- * is_palindrome - is function that looks for palindrome
+ * is_palindrome_helper - is a helper function
  * @s: is the parameter passed
  * Return: always 0
+ * @start: is the initial
+ * @end: is the terminal
+ */
+
+int is_palindrome_helper(char *s, int start, int end)
+	{
+	if (start >= end)
+	{
+	return (1);
+	}
+	if (s[start] != s[end])
+	{
+	return (0);  /* Not a palindrome */
+	}
+
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - is function that looks for palindrome
+ * @s: the string to be checked
+ * Return: 1 if the string is a palindrome, 0 otherwise
  */
 
 int is_palindrome(char *s)
 {
-	char *start = s;
-	char *end = s + strlen(s) - 1;
+	int length = strlen(s);
 
-	if (*s == '\0')
-	{
-		return (1);
-	}
-
-	while (start < end)
-	{
-		if (*start != *end)
-		{
-			return (0);
-		}
-		start++;
-		end--;
-	}
-
-	return (1);
+	return (is_palindrome_helper(s, 0, length - 1));
 }
