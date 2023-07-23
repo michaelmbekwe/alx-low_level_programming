92b16d956f57f17cfe4dede045693749d92091a4
michael mbekwe <mbekwemichael1@gmail.com>
2023-07-23 12:49:23 +0000

    argument passed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - is the entry point
+ *@argc: is the argument count
+ *@argv: is the argument vector
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	{
+
+	char *programName = argv[0];
+
+	char *lastSlash = strrchr(programName, '/');
+
+	if (lastSlash != NULL)
+	{
+		programName = lastSlash + 1;
+	}
+
+	printf(" %s\n", programName);
+	}
+	return (0);
+}
